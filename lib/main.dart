55a11d3cf07d36d7dcd6e426f6ebf62756c1bb6a
Ukkey <r.ukita@ukihome.com>
2023-11-14 21:52:13 +0900

    Add TextFormField

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,20 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final formKey = GlobalKey<FormState>();
-    const values = [1, 2, 3, 4, 5, 11, 12, 13, 14, 15];
-    var currentRandomNumberPickerValue = values[0];
-    var currentSimpleDropdownValue = values[0];
+
+    var currentText = 'Hello';
+
+    const numbers = [1, 2, 3, 4, 5, 11, 12, 13, 14, 15];
+    var currentNumber = numbers[0];
+
+    const colors = [
+      Colors.red,
+      Colors.green,
+      Colors.blue,
+      Colors.yellow,
+      Colors.purple,
+    ];
+    var currentColor = colors[0];
 
     return Scaffold(
       body: Center(
@@ -28,32 +39,56 @@ class Home extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              // テキスト入力欄
+              SizedBox(
+                width: 200,
+                child: TextFormField(
+                  initialValue: currentText,
+                  onChanged: (value) => currentText = value,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return '必須項目です。';
+                    }
+                    if (value.length <= 3) {
+                      return '3文字以上入力してください。';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(height: 20),
+              // ランダム数字選択欄
               RandomNumberPickerForm(
-                values: values,
-                initialValue: currentRandomNumberPickerValue,
-                onChanged: (value) => currentRandomNumberPickerValue = value,
+                values: numbers,
+                initialValue: currentNumber,
+                onChanged: (value) => currentNumber = value,
                 validator: (value) {
                   if (value == null) {
-                    return 'Please select a value.';
+                    return '必須項目です。';
                   }
                   if (value % 2 == 0) {
-                    return 'Even numbers are not allowed.';
+                    return '偶数は選択できません。';
                   }
                   return null;
                 },
               ),
               const SizedBox(height: 20),
+              // カラー選択欄
               SimpleDropdownForm(
-                values: values,
-                initialValue: currentSimpleDropdownValue,
-                onChanged: (value) => currentSimpleDropdownValue =
-                    value ?? currentSimpleDropdownValue,
+                values: colors,
+                initialValue: currentColor,
+                onChanged: (value) => currentColor = value ?? currentColor,
+                itemBuilder: (context, value) => Container(
+                  width: 100,
+                  height: 20,
+                  color: value,
+                ),
                 validator: (value) {
                   if (value == null) {
-                    return 'Please select a value.';
+                    return '必須項目です。';
                   }
-                  if (value % 2 == 1) {
-                    return 'Odd numbers are not allowed.';
+                  if (value == Colors.red) {
+                    return '赤は選択できません。';
                   }
                   return null;
                 },
@@ -68,10 +103,13 @@ class Home extends StatelessWidget {
                     duration: const Duration(milliseconds: 300),
                     backgroundColor: Theme.of(context).primaryColor,
                     content: Text(
-                        'Submitted!: $currentRandomNumberPickerValue, $currentSimpleDropdownValue'),
+                      'テキスト: $currentText, '
+                      '数字: $currentNumber, '
+                      'カラー: $currentColor',
+                    ),
                   ));
                 },
-                child: const Text('Submit'),
+                child: const Text('確定'),
               ),
             ],
           ),
